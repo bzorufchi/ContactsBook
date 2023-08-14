483a12e671c92dc014f997ace44d7e47e209c0f2
bzorufchi <b.zorufchi@gmail.com>
2023-08-14 09:27:39 +0330

    Add project files.

---
+++ ContactsBook/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+using contactsbook;
+using System.Collections.Generic;
+
+
+
+
+ContactBook book = new ContactBook();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("****************************************************");
+    Console.WriteLine("Enter your choice:\n1)Add Contact\n2)Show All\n3)search contact\n4)Update");
+
+    Console.Write("Enter key between 1 - 2 - 3 - 4:");
+    string key = Console.ReadLine();
+    int keyNumber = Convert.ToInt32(key);
+    Console.WriteLine("****************************************************");
+    if (keyNumber == 1 || keyNumber == 2 || keyNumber == 3 || keyNumber == 4)
+    {
+        Console.Clear();
+        switch (keyNumber)
+        {
+            case 1:
+                ContactClass contact = book.Add();
+                book.ShowOneContact(contact);
+
+                break;
+            case 2:
+                book.ShowAllContacts();
+                break;
+
+            case 3:
+                book.SearchContact();
+
+                break;
+            case 4:
+                book.ShowAllContacts();
+                Console.WriteLine("_______________________________________________________________");
+                book.update();
+
+                break;
+            default:
+                Console.WriteLine("end go back to menu");
+                break;
+        }
+        Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("!!! Please enter number between 1 and 2 and 3  and 4!!!");
+        Console.ReadKey();
+
+    }
+}
+
+
+
+
+
+
---
+++ ContactsBook/contactsbook.cs
@@ -0,0 +1,94 @@
+﻿using contactsbook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ContactBook
+{
+    public List<ContactClass> contatcs { get; set; } = new List<ContactClass>();
+    public ContactClass Add()
+    {
+        ContactClass c = new ContactClass();
+        Console.Write("Enter Name");
+        c.Name = Console.ReadLine();
+        Console.Write("Enter Last Name");
+        c.LastName = Console.ReadLine();
+        Console.Write("Enter phonnumber");
+        c.PhoneNumber = Console.ReadLine();
+
+        c.FullName = c.Name + "-" + c.LastName;
+
+        contatcs.Add(c);
+        return c;
+
+    }
+    public void ShowOneContact(ContactClass c)
+    {
+        Console.WriteLine($"FullName:{c.FullName} ** PhoneNumber: {c.PhoneNumber}");
+    }
+    public void ShowAllContacts()
+    {
+        foreach (ContactClass c in contatcs)
+        {
+            //Console.WriteLine($"FullName:{c.FullName} ** PhoneNumber: {c.PhoneNumber}");
+            ShowOneContact(c);
+        }
+
+    }
+    public ContactClass SearchContact()
+    {
+        Console.Write("shomare vared namaiid:");
+        string phonenumber = Console.ReadLine();
+        bool find = false;
+        ContactClass contact = new ContactClass();
+        for (int i = 0; i < contatcs.Count; i++)
+        {
+            if (phonenumber == contatcs[i].PhoneNumber)
+            {
+                contact = contatcs[i];
+                ShowOneContact(contatcs[i]);
+                find = true;
+                break;
+            }
+        }
+        if (!find)
+        {
+            Console.WriteLine("not found");
+        }
+        return contact;
+
+    }
+    public void update()
+    {
+
+        ContactClass contact = new ContactClass();
+        contact = SearchContact();
+
+        if (contact == null)
+        {
+            Console.WriteLine("not Found!!!!!! please try again.");
+            return;
+        }
+        string oldphonenumber = contact.PhoneNumber;
+        Console.WriteLine("please enter new phone number: ");
+        contact.PhoneNumber = Console.ReadLine();
+
+        for (int i = 0; i < contatcs.Count; i++)
+        {
+            if (oldphonenumber == contatcs[i].PhoneNumber)
+            {
+                contatcs[i].PhoneNumber = contact.PhoneNumber;
+                break;
+            }
+        }
+        Console.WriteLine($"shomare {oldphonenumber} karbar{contact.FullName} be shomare {contact.PhoneNumber} ba movafaghiyat taghir yaft.");
+
+    }
+
+}
+  
+
+
+
